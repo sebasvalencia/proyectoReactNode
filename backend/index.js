c6aa9bbe4d80e68475de081cf5163b80017250a1
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-10-11 15:33:47 -0500

    Se crea el enterpoint

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
+'use strict';
 
+const mongoose = require('mongoose');
+const app = require('./app');
+const config = require('./config');
+
+mongoose.connect(config.db, (err, res) =>{
+    if(err){
+        return console.log(`Error al conectar a la BD ${err}`);
+    }else{
+        console.log(`Conexió a la BD establecida...`);
+        app.listen(config.port, ()=>{
+            console.log(`API REST corriendo en http:localhost:${config.port}`);
+        });
+    }
+});
