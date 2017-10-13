e16a037c7b22113001d3073aa66cfbcf35ee2b6f
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-10-13 16:09:14 -0500

    Se agrega CORS para poder hacer peticiones desde React

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const api = require('./routes/routes');
 
+api.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-Type, Accept");
+});
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/api', api);
